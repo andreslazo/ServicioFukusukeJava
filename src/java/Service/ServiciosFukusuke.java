fce2d6b814ce9871deae778873e909b717cfa706
Andrés Lazo <am.lazo@outlook.com>
2017-06-12 00:16:30 -0400

    Muchos servicios ya son funcionales

--- src/java/Service/ServiciosFukusuke.java
+++ src/java/Service/ServiciosFukusuke.java
@@ -6,7 +6,11 @@
 package Service;
 
 import DAO.ClientesDAO;
-import java.util.ArrayList;
+import DAO.PromocionesDAO;
+import DAO.VentasDAO;
+import entity.Ventas;
+import java.sql.Date;
+import org.json.JSONArray;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -24,39 +28,59 @@ public class ServiciosFukusuke {
      */
     @WebMethod(operationName = "getDatosPedido")
     public String getDatosPedido(@WebParam(name = "codigoPedido") int codigoPedido) {
-        return "Los datos del pedido son " + codigoPedido ;
+        VentasDAO ventas=new VentasDAO();
+        JSONArray arreglo;
+        arreglo=ventas.Leer(codigoPedido);
+        return arreglo.toString();
     }
     
     @WebMethod(operationName = "getDatosCliente")
-    public static String getDatosCliente(@WebParam(name = "idCliente") int idCliente) {
+    public String getDatosCliente(@WebParam(name = "usuario") String usuario) {
         ClientesDAO cliente=new ClientesDAO();
-        ArrayList<String> arreglo;
-        arreglo=cliente.Leer(idCliente);
-        return "Los datos del cliente con id: "+idCliente+" son " + arreglo;
+        JSONArray arreglo;
+        arreglo=cliente.Leer(usuario);
+        return arreglo.toString();
     }
     
     @WebMethod(operationName = "setPedido")
     public boolean setPedido(@WebParam(name = "user") String user,@WebParam(name = "idMenu") int idMenu) {
-        return false;
+        //sacar precio del menu listo
+        //sacar sucursal del menu listo
+        //sacar el id del cliente listo
+        try{
+            PromocionesDAO menues=new PromocionesDAO();
+            ClientesDAO clientes=new ClientesDAO();
+            int precio=menues.ObtenerPrecio(idMenu);
+            int idSucursal=menues.ObtenerSucursal(idMenu);
+            int idCliente=clientes.ObtenerId(user);
+            Date fecha=new Date(1995, 8, 24);
+            Ventas venta=new Ventas(3, 12334, precio, 0, 0, fecha , fecha , 1, idSucursal, idCliente);
+            
+            VentasDAO ventas=new VentasDAO();
+            return ventas.Insertar(venta);
+        }catch(Exception e){
+            return false;
+        }
+        
     }
     
     @WebMethod(operationName = "getListaMenu")
     public String getListaMenu() {
-        return "Proximamente podra ver las lista de menus";
+        PromocionesDAO promociones=new PromocionesDAO();
+        JSONArray arreglo;
+        arreglo=promociones.LeerTodo();
+        return arreglo.toString();
     }
     
     @WebMethod(operationName = "setEstadoPedido")
     public boolean setEstadoPedido(@WebParam(name = "codigoPedido") int codigoPedido,@WebParam(name = "estado") int estado) {
+        //TO DO
         return false;
     }
     
     @WebMethod(operationName = "getEstadoPedido")
-    public String[] getEstadoPedido(@WebParam(name = "codigoPedido") int codigoPedido) {
-        //Pruebas de arreglos en String para WebServices
-        String[] estados= new String[2];
-        estados[0]=String.valueOf(codigoPedido);
-        estados[1]=String.valueOf(codigoPedido);
-        
-        return estados;
+    public int getEstadoPedido(@WebParam(name = "codigoPedido") int codigoPedido) {
+        //TO DO
+        return 0;
     }
 }
